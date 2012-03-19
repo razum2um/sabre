35835dc744bb5ffdbf202752106c8b829e1393a7
John Perkins <jeperkins4@gmail.com>
2012-03-19 14:41:38 -0400

    Added room rate range filter

--- lib/sabre/hotel.rb
+++ lib/sabre/hotel.rb
@@ -5,32 +5,36 @@ module Sabre
       client.http.headers["Content-Type"] = "text/xml;charset=UTF-8"
       response = client.request(:ota_hotel_avail_rq, { 'xmlns' => 'http://webservices.sabre.com/sabreXML/2003/07', 'xmlns:xs' => 'http://www.w3.org/2001/XMLSchema', 'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance', 'TimeStamp' => Time.now.strftime('%Y-%m-%dT%H:%M:%S'), 'Version' => '2003A.TsabreXML1.11.1'}) do
         soap.namespaces["xmlns:SOAP-ENV"] = "http://schemas.xmlsoap.org/soap/envelope/"
-	soap.namespaces["xmlns:eb"] = "http://www.ebxml.org/namespaces/messageHeader"
-	soap.namespaces["xmlns:xlinx"] = "http://www.w3.org/1999/xlink"
-	#soap.namespaces["xmlns"] = 'http://www.opentravel.org/OTA/2002/08'
-	soap.version = 1
-	soap.header = session.header('Hotel Availability','sabreXML','OTA_HotelAvailLLSRQ')
-	soap.body = {
+        soap.namespaces["xmlns:eb"] = "http://www.ebxml.org/namespaces/messageHeader"
+        soap.namespaces["xmlns:xlinx"] = "http://www.w3.org/1999/xlink"
+        #soap.namespaces["xmlns"] = 'http://www.opentravel.org/OTA/2002/08'
+        soap.version = 1
+        soap.header = session.header('Hotel Availability','sabreXML','OTA_HotelAvailLLSRQ')
+        soap.body = {
           'POS' => { 'Source' => "", :attributes! => { 'Source' => { 'PseudoCityCode' => session.ipcc } } },
-	     'AvailRequestSegments' => {
-		'AvailRequestSegment' => {
-                    'StayDateRange' => '', 'RoomStayCandidates' => {
-                    'RoomStayCandidate' => { 'GuestCounts' => { 'GuestCount' => '', :attributes! => { 'GuestCount' => { 'Count' => guest_count } } } } 
-                    }, 'HotelSearchCriteria' => {
-			'Criterion' => { 
-                            'HotelAmenity' => amenities, 'HotelRef' => '', 'RefPoint' => 'G', :attributes! => {
-                                'HotelRef' => { 'Latitude' => latitude, 'Longitude' => longitude }, 
-				'RefPoint' => { 'GEOCodeOnly' => 'true', 'LocationCode' => 'R' },
-			    } 
-                        }
-		    }, :attributes! => { 
-			'StayDateRange' => { 'Start' => start_time.strftime('%m-%d'), 'End' => end_time.strftime('%m-%d') }, 
-			'HotelSearchCriteria' => { 'NumProperties' => 20 } 
-		    }
-		 }
-	     }
-	}
-    end
+          'AvailRequestSegments' => {
+            'AvailRequestSegment' => {
+              'StayDateRange' => '', 
+              'RatePlanCandidates' => {
+                'RateRange' => '', :attributes! => { 'RateRange' => { 'CurrencyCode' => 'USD', 'Max' => '1000.00', 'Min' => '20.00' }}
+              }, 'RoomStayCandidates' => {
+                 'RoomStayCandidate' => { 'GuestCounts' => { 'GuestCount' => '', :attributes! => { 'GuestCount' => { 'Count' => guest_count } } } } 
+              }, 'HotelSearchCriteria' => {
+                 'Criterion' => { 
+                   'HotelAmenity' => amenities, 'HotelRef' => '', 'RefPoint' => 'G', :attributes! => {
+                     'HotelRef' => { 'Latitude' => latitude, 'Longitude' => longitude }, 
+                     'RefPoint' => { 'GEOCodeOnly' => 'true', 'LocationCode' => 'R' },
+                   } 
+                 }
+              }, :attributes! => { 
+                'StayDateRange' => { 'Start' => start_time.strftime('%m-%d'), 'End' => end_time.strftime('%m-%d') }, 
+                'RatePlanCandidates' => { 'SuppressRackRate' => 'false' },
+                'HotelSearchCriteria' => { 'NumProperties' => 20 } 
+              }
+            }
+          }
+        }
+      end
   end
 
     def self.find_by_iata(session, start_time, end_time, iata_city_code, guest_count, amenities = [])
